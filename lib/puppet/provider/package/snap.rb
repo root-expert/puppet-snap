ba896c43044ec99cc16331cf2af4d0a3734c73d8
Christos Papageorgiou <christos.papageorgioy@gmail.com>
2021-09-12 17:18:24 +0300

    Fix package prefetching
    Signed-off-by: Christos Papageorgiou <christos.papageorgioy@gmail.com>

--- lib/puppet/provider/package/snap.rb
+++ lib/puppet/provider/package/snap.rb
@@ -2,7 +2,7 @@ require 'puppet/provider/package'
 require 'socket'
 require 'json'
 
-Puppet::Type.type(:package).provide :snap, :parent => Puppet::Provider::Package do
+Puppet::Type.type(:package).provide :snap, parent: Puppet::Provider::Package do
   desc "Package management via Snap.
 
     This provider supports the `install_options` attribute, which allows snap's flags to be
@@ -11,12 +11,18 @@ Puppet::Type.type(:package).provide :snap, :parent => Puppet::Provider::Package 
     This provider supports the `uninstall_options` attribute, which allows snap's flags to be
     passed to Snap. Namely `purge`."
 
-  commands :snap => "/usr/bin/snap"
   has_feature :install_options
   has_feature :uninstall_options
 
   def self.instances
-    installed_snaps
+    instances = []
+    snaps = installed_snaps
+
+    snaps.each do |snap|
+      instances << new(name: snap['name'], ensure: snap['version'], provider: 'snap')
+    end
+
+    instances
   end
 
   def install
@@ -35,47 +41,48 @@ Puppet::Type.type(:package).provide :snap, :parent => Puppet::Provider::Package 
     uninstall
   end
 
-  def call_api(method, url, data = nil)
+  def self.call_api(method, url, data = nil)
     request = "#{method} #{url} HTTP/1.1\r\n" \
+"Host: localhost\r\n" \
 "Accept: application/json\r\n" \
 "Content-Type: application/json\r\n"
 
     if method == 'POST'
       post_data = data.to_json.to_s
       # Add Content-Length Header since we have some payload
-      request.concat(request, "Content-Length: #{post_data.bytesize}\r\n\r\n#{post_data}")
+      request << "Content-Length: #{post_data.bytesize}\r\n\r\n#{post_data}"
     end
     # HTTP headers should end with blank line
-    request.concat(request, "\r\n")
+    request << "\r\n"
 
     res = nil
     UNIXSocket.open('/run/snapd.socket') do |socket|
       socket.write(request)
-      reply = socket.recv
+      reply = socket.recv(8192)
 
       # Strip HTTP headers
       res = reply.split(%r{\r\n\r\n})[1]
     end
 
-    raise Puppet::Error('There was an error calling Snap API') if res.nil?
-    res
+    raise Puppet::Error, 'There was an error calling Snap API' if res.nil?
+    JSON.parse(res)
   end
 
-  def installed_snaps
-    res = call_api('GET', '/v2/snaps/')
+  def self.installed_snaps
+    res = call_api('GET', '/v2/snaps')
 
-    if res['status-code'] != 200
-      raise Puppet::Error(format('Could not find installed snaps %s', res['result']))
+    unless [200, 404].include?(res['status-code'])
+      raise Puppet::Error, format('Could not find installed snaps (code: %s)', res['status-code'])
     end
 
-    res.map { |snap| snap.is_a?(Hash) ? snap['name'] : next }
+    res['result'].map { |hash| hash.slice('name', 'version') } if res['status-code'] == 200
   end
 
   # Helper method to return the change ID from a asynchronous request response.
-  def get_id_from_async_req(request)
+  def self.get_id_from_async_req(request)
     # If the request failed raise an error
     if request['type'] == 'error'
-      raise Puppet::Error(format('Request failed with %s', request['result']['message']))
+      raise Puppet::Error, format('Request failed with %s', request['result']['message'])
     end
 
     request['change']
@@ -84,7 +91,7 @@ Puppet::Type.type(:package).provide :snap, :parent => Puppet::Provider::Package 
   # Get the status of a change
   #
   # @param id The change ID to search for.
-  def get_status(id)
+  def self.get_status(id)
     call_api('GET', "/v2/changes/#{id}")
   end
 
@@ -92,7 +99,7 @@ Puppet::Type.type(:package).provide :snap, :parent => Puppet::Provider::Package 
   # been completed.
   #
   # @param id The change ID to search for.
-  def complete(id)
+  def self.complete(id)
     completed = false
     until completed
       res = get_status(id)
@@ -101,11 +108,11 @@ Puppet::Type.type(:package).provide :snap, :parent => Puppet::Provider::Package 
         # Still running
         next
       when 'Abort', 'Hold', 'Error'
-        raise Puppet::Error(format('Error while executing the request %s', res))
+        raise Puppet::Error, format('Error while executing the request %s', res)
       when 'Done'
         completed = true
       else
-        raise Puppet::Error(format('Unknown status %s', res))
+        raise Puppet::Error, format('Unknown status %s', res)
       end
 
       # Wait a little bit before hitting the API again!
@@ -113,7 +120,7 @@ Puppet::Type.type(:package).provide :snap, :parent => Puppet::Provider::Package 
     end
   end
 
-  def generate_request(action, options)
+  def self.generate_request(action, options)
     request = { 'action' => action }
 
     if options
@@ -133,7 +140,7 @@ Puppet::Type.type(:package).provide :snap, :parent => Puppet::Provider::Package 
     request
   end
 
-  def modify_snap(action, name, options)
+  def self.modify_snap(action, name, options)
     req = generate_request(action, options)
     response = call_api('POST', "/v2/snaps/#{name}", req)
     change_id = get_id_from_async_req(response)
